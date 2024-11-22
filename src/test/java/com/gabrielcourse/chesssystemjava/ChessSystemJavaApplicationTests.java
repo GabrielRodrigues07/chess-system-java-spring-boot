93d1c069ca98b3e6118e96da3290da3a49c6502d
Gabriel <grodrigues256@gmail.com>
2024-11-22 10:09:58 -0300

    feat(2024-11-22) Project Creation

---
+++ src/test/java/com/gabrielcourse/chesssystemjava/ChessSystemJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielcourse.chesssystemjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessSystemJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
