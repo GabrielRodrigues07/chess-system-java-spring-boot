04f3410fb9f38182e4aaac964e1c955ca918af2b
Gabriel <grodrigues256@gmail.com>
2024-11-22 11:11:57 -0300

    feat(2024-11-22) BoardException and defensive programming

---
+++ src/main/java/com/gabrielcourse/chesssystemjava/boardgame/exceptions/BoardException.java
@@ -0,0 +1,8 @@
+package com.gabrielcourse.chesssystemjava.boardgame.exceptions;
+
+public class BoardException extends RuntimeException {
+
+    public BoardException(String message) {
+        super(message);
+    }
+}
