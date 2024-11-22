8b678f8874cf1932b2e9e5874d2b07bd718fae45
Gabriel <grodrigues256@gmail.com>
2024-11-22 12:31:03 -0300

    feat(2024-11-22) Little improvement in board printing

--- src/main/java/com/gabrielcourse/chesssystemjava/chess/ChessPiece.java
+++ src/main/java/com/gabrielcourse/chesssystemjava/chess/ChessPiece.java
@@ -3,12 +3,14 @@ package com.gabrielcourse.chesssystemjava.chess;
 import com.gabrielcourse.chesssystemjava.boardgame.Board;
 import com.gabrielcourse.chesssystemjava.boardgame.Piece;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Setter
+@Getter
 public class ChessPiece extends Piece {
 
     private Color color;
