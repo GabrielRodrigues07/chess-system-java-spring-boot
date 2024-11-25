289dd6a5a984a0a447a49b97356556add8586853
Gabriel <grodrigues256@gmail.com>
2024-11-25 11:26:06 -0300

    feat(2024-11-25) Piece move count

--- src/main/java/com/gabrielcourse/chesssystemjava/chess/ChessPiece.java
+++ src/main/java/com/gabrielcourse/chesssystemjava/chess/ChessPiece.java
@@ -17,10 +17,19 @@ import java.util.Objects;
 public abstract class ChessPiece extends Piece {
 
     private Color color;
+    private int moveCount;
 
     public ChessPiece(Board board, Color color) {
         super(board);
         this.color = color;
+    }
+
+    public void increaseMoveCount() {
+        moveCount++;
+    }
+
+    public void decreaseMoveCount() {
+        moveCount--;
     }
 
     protected boolean isThereOpponentPiece(Position position) {
